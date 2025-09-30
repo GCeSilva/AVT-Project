e998b70d9546cdd7e4d697952abb47c27aaf7c6b
GCeSilva <goncalo.c.silva@hotmail.com>
2025-09-30 17:54:03 +0100

    Alpha Blending added

--- MyAVT_Template/lightDemo.cpp
+++ MyAVT_Template/lightDemo.cpp
@@ -572,6 +572,8 @@ int main(int argc, char **argv) {
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_MULTISAMPLE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glClearColor(0.5, 0.6, 0.7, 1.0f);
 
 	printf ("Vendor: %s\n", glGetString (GL_VENDOR));
